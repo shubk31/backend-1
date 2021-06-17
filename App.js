89fd0b46d4b3af7684690b5220118f12b11c7057
shubk31 <shubhamsharma2.cse23@jecrc.ac.in>
2021-06-17 19:07:01 +0530

    new

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ const router = Router();
 const authController = require('./controllers/authController');
 const itemController = require('./controllers/itemController');
 const cartController = require('./controllers/cartController');
-const orderController = require('../controllers/orderController');
+const orderController = require('./controllers/orderController');
 
 const auth = require('./middlewares/auth');
 
@@ -46,27 +46,27 @@ app.post('/login', authController.login);
 app.get('/user', auth, authController.get_user);
 
 //------------------------Items----------------------------------------
-app.get("/items", (req, res) => {
-    res.render("additems");
-});
+// app.get("/items", (req, res) => {
+//     res.render("additems");
+// });
 
-app.get('/getitems',itemController.get_items);
-//admin
-app.post('/items',itemController.post_item);
-app.put('/items/:id',itemController.update_item);
+// app.get('/getitems',itemController.get_items);
+// //admin
+// app.post('/items',itemController.post_item);
+// app.put('/items/:id',itemController.update_item);
 // app.delete('/deleteitems/:id',itemController.delete_item);
 
 //------------------------Cart----------------------------------------
-app.get("/cart/:id", (req, res) => {
-    res.render("addcartitem");
-});
-app.get('/cart/:id',cartController.get_cart_items);
-app.post('/cart/:id',cartController.add_cart_item);
-router.delete('/cart/:userId/:itemId',cartController.delete_item);
+// app.get("/cart/:id", (req, res) => {
+//     res.render("addcartitem");
+// });
+// app.get('/cart/:id',cartController.get_cart_items);
+// app.post('/cart/:id',cartController.add_cart_item);
+// app.delete('/cart/:userId/:itemId',cartController.delete_item);
 //------------------------Order----------------------------------------
 
-app.get('/order/:id',orderController.get_orders);
-app.post('/order/:id',orderController.checkout);
+// app.get('/order/:id',orderController.get_orders);
+// app.post('/order/:id',orderController.checkout);
 
 //----------------------------------------------------------------
 app.listen(process.env.PORT || 3033, function (err) {
